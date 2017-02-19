e872599d0afcc5360740b703d4ea50cb3365acdc
Aleksander Dash <adash@stanford.edu>
2017-02-19 08:35:56 -0800

    Fixed scrolling
    More stuff works now!

--- TreeHacks/listen/static/listen/listen.js
+++ TreeHacks/listen/static/listen/listen.js
@@ -1,5 +1,6 @@
 const DEFAULT_DELAY = 1000;
 
+/*
 $('a[href^="#"]').on('click', function(event) {
     var target = $(this.getAttribute('href'));
     if( target.length ) {
@@ -9,3 +10,36 @@ $('a[href^="#"]').on('click', function(event) {
         }, DEFAULT_DELAY);
     }
 });
+*/
+
+$(document).ready(function(){
+	//$('a[href^="#"]').bind('click.smoothscroll',function (e) {
+	$('a[href="#langSelect"]').bind('click.smoothscroll',function (e) {
+	    e.preventDefault();
+	 
+	    var target = this.hash,
+	    $target = $(target);
+	 
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, DEFAULT_DELAY, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
+/*
+$(function(){
+	
+	$('.menu').click(function(e) {
+		$('html').toggleClass('menu-active');
+		e.preventDefault();
+	});
+	
+	$('.menu-wrapper a').click(function(e) {
+		$('html').removeClass('menu-active');
+	});
+	
+	$('.menu-close').click(function(e) {
+		$('html').removeClass('menu-active');
+	});
+});*/
